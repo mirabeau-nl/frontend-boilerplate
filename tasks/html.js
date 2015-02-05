199b66db4eab0037bcc9a4622b9584ec86116011
branneman <branmovic@gmail.com>
2015-02-05 15:31:40 +0100

    Swig/HTML compiler and watcher now reload browsersync

--- tasks/html.js
+++ tasks/html.js
@@ -1,10 +1,11 @@
-var gulp = require('gulp');
-var swig = require('gulp-swig');
+var gulp        = require('gulp');
+var swig        = require('gulp-swig');
+var browsersync = require('browser-sync');
 
 // Define paths
 var globTemplates  = global.paths.src + '/templates/**/*.html';
 var globLayout     = global.paths.src + '/layout/*.html';
-var globComponents = global.paths.src + '/components/**/html/*.html';
+var globComponents = global.paths.src + '/components/**/*.html';
 var dirDist        = global.paths.dist + '/templates';
 
 /**
@@ -13,7 +14,8 @@ var dirDist        = global.paths.dist + '/templates';
 module.exports.compile = function() {
     gulp.src(globTemplates)
         .pipe(swig({ defaults: { cache: false } }))
-        .pipe(gulp.dest(dirDist));
+        .pipe(gulp.dest(dirDist))
+        .pipe(browsersync.reload({ stream: true }));
 };
 
 /**
