ff66145b15f3d326ea3489d854fbec9c07568c4b
Bram Swier <bswier@mirabeau.nl>
2015-12-05 14:12:26 +0100

    Implemented a structure for polyfill checks.
    Implemented a structure for polyfill checks, which can be commented out
    if you need it in your project. The classList polyfill is provided by
    default because IE9 doesn’t support classList.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -6,6 +6,17 @@ require.config({
     }
 });
 
-require(['conditioner'], function(conditioner) {
+/**
+ * Feature detections to include polyfills if necessary.
+ * Uncomment the code below to activate it in your project and expand on it to your needs.
+ */
+
+//let polyfills = [];
+//
+//if (!('classList' in document.documentElement)) {
+//    polyfills.push('polyfill/classList');
+//}
+
+require(['conditioner'].concat(typeof polyfills !== 'undefined' ? polyfills : ''), function(conditioner) {
     conditioner.init();
 });
