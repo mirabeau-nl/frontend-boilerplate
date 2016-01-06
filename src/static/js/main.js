fc647154d9ea195bd4a1c2ab5b66c1df1eebb572
Bram Swier <bswier@mirabeau.nl>
2016-01-06 22:58:28 +0100

    commented polyfill check
    commented polyfill check

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -12,9 +12,9 @@ let polyfills = [];
  * Uncomment the feature detection below to activate the polyfill in your project and expand on it to your needs.
  */
 
-if (!('classList' in document.documentElement)) {
-    polyfills.push('polyfill/classList');
-}
+//if (!('classList' in document.documentElement)) {
+//    polyfills.push('polyfill/classList');
+//}
 
 require(['conditioner'].concat(polyfills), function(conditioner) {
     conditioner.init();
