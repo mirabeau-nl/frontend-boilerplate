e03168405342fb328a5f5b84ffde544d0d70ff43
Sanjay Twisk <sanjay@twisk-interactive.nl>
2016-12-08 10:41:12 +0100

    Fix conditioner monitors issue when using browserify (#39)
    - Changed the custom conditioner loader for commonjs / es2015 modules
    - Included comments in bundle.js for loading the monitors

--- src/static/js/bundle.js
+++ src/static/js/bundle.js
@@ -1,16 +1,30 @@
 /**
-* Browserify entry for bundling
-* Modules are bundled in js gulp task
-* !!! This file is NOT used when using RequireJS
-*/
+ * Browserify entry for bundling
+ * Modules are bundled in js gulp task
+ * !!! This file is NOT used when using RequireJS
+ */
 import classList from 'polyfill/classList'; // eslint-disable-line no-unused-vars
 import conditioner from 'conditioner-js';
+
+/**
+ * When using conditioners monitors
+ * import the required monitors:
+ */
+//
+// import 'conditioner-js/dist/min/monitors/media';
 
 // Setup conditioner loader to load modules from bundle
 conditioner.setOptions({
     loader: {
-        require: (paths, callback) => callback(require(paths).default),
+        require: (paths, callback) => {
+            const module = require(paths);
+
+            return callback(module.default ? module.default : module);
+        },
         toUrl: path => path
+    },
+    paths: {
+        monitors: 'conditioner-js/dist/min/monitors/'
     }
 });
 
