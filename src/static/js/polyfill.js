a492cd908b1a5cb2f208c4f685461773f21d39e4
A. Wolkers <awolkers@mirabeau.nl>
2018-10-04 09:04:51 +0200

    Modernize default layout

--- src/static/js/polyfill.js
+++ src/static/js/polyfill.js
@@ -3,7 +3,4 @@ import 'core-js/fn/dom-collections/iterator';
 import 'core-js/fn/array/from';
 import 'core-js/fn/object/assign';
 import 'core-js/es6/promise';
-
-// from vendor folder
-import 'vendor/eligrey/classList';
-import 'vendor/brettz9/html5-dataset';
+import 'eligrey-classlist-js-polyfill';
