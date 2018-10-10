41e056fa99f09f414d14b25a618dc8ef2a50a00c
A. Wolkers <awolkers@mirabeau.nl>
2018-10-10 09:14:28 +0200

    update-default-layout: import classlist polyfill from npm module instead of embedded file.

--- src/static/js/polyfill.js
+++ src/static/js/polyfill.js
@@ -3,6 +3,4 @@ import 'core-js/fn/dom-collections/iterator';
 import 'core-js/fn/array/from';
 import 'core-js/fn/object/assign';
 import 'core-js/es6/promise';
-
-// from vendor folder
-import 'vendor/eligrey/classList';
+import 'eligrey-classlist-js-polyfill';
